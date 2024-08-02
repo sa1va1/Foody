4c1c0a76c45330598682ecf04f0cec6366145d1b
Sarvar <s3266321@gmail.com>
2024-08-02 18:36:09 +0500

    Update data.js

---
+++ 3-dars/src/components/static/data.js
@@ -0,0 +1,25 @@
+
+
+const customers = [
+    {
+        id: 1,
+        description: "You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change.",
+        image: "/assets/images/customer1.png",
+        Name: 'Leslie Alexander',
+        role: 'Founder',
+    },
+    {
+        id: 2,
+        description: 'Simply the best. Better than all the rest. I`d recommend this product to beginners and advanced users.',
+        image: '/assets/images/second-customer.png',
+        Name: 'Jacob Jones',
+        role: 'Co-Founder',
+    },
+    {
+        id: 3,
+        description: 'Simply the best. Better than all the rest. I’d recommend this product to beginners and advanced users.',
+        image: '/assets/images/last-customer.png',
+        Name: 'Jenny Wilson',
+        role: 'Chief Marketing Officer',
+    }
+]
